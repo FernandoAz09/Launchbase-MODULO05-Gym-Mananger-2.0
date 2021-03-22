b73bb879af7b1260507b8db71bf62f2ff4c705fa
Fernando Azevedo <56461093+FernandoAz09@users.noreply.github.com>
2021-03-22 14:41:53 -0300

    Update Member.js

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -24,7 +24,7 @@ module.exports = {
                 weight,
                 height,
                 instructor_id
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7,$8, $9)
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING id
         `
 
